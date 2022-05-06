df4cda2f7aae82d6b5c50fd66c6cec4bbebf57fd
opilane <opilane@HP-CZC6297B8F>
2022-05-06 12:22:38 +0300

    qq

---
+++ Elemendide_App/europariigid.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Elemendide_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class europariigid : ContentPage
+    {
+        public ObservableCollection<riigid> riigi { get; set; }
+        Label lbl_list;
+        ListView list;
+        Button Kustuta_btn, lisa_btn;
+        public europariigid()
+        {
+            riigi = new ObservableCollection<riigid>
+            {
+                new riigid { Nimetus = "Finland", Pealinn = "Samsung", Elanikkond = 1249, Pilt = "s22.png" },
+                new riigid { Nimetus = "Estonia", Pealinn = "Samsung", Elanikkond = 1249, Pilt = "s22.png" },
+                new riigid { Nimetus = "Venemaa", Pealinn = "Samsung", Elanikkond = 1249, Pilt = "s22.png" },
+                new riigid { Nimetus = "Ukraine", Pealinn = "Samsung", Elanikkond = 1249, Pilt = "s22.png" }
+            };
+        }
+    }
+}
---
+++ Elemendide_App/riigid.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elemendide_App
+{
+    public class riigid
+    {
+        public string Nimetus { get; set; }
+        public string Pealinn { get; set; }
+        public int Elanikkond { get; set; }
+        public string Pilt { get; set; }
+    }
+}
